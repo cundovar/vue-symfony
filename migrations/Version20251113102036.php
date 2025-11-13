ed759c47b7c5851f263612e2983eb9a5eff5b602
Facundo VARAS <102789450+cundovar@users.noreply.github.com>
2025-11-13 11:22:44 +0100

    suite

--- migrations/Version20251113102036.php
+++ migrations/Version20251113102036.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20251018204805 extends AbstractMigration
+final class Version20251113102036 extends AbstractMigration
 {
     public function getDescription(): string
     {
